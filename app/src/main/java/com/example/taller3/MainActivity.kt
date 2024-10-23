222f5a9ae64114f79a1b57fd3ce601e7c6c8f1e0
Sira <sgonzmad@myuax.com>
2024-10-23 09:05:46 +0200

    implementar almacenamiento interno y externo

--- app/src/main/java/com/example/taller3/MainActivity.kt
+++ app/src/main/java/com/example/taller3/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.taller3
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -14,15 +18,61 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import java.util.*
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Alamcenamiento externo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Usa el almacenamiento específico de la app
+            val externalDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+            val filePath = "${externalDir?.absolutePath}/datos_externos.txt"
+            AlmacenamientoUtils.guardarEnAlmacenamientoExterno(filePath, "Algunos datos de prueba")
+
+            val datosExternos = AlmacenamientoUtils.leerDesdeAlmacenamientoExterno(filePath)
+            Log.d("Externo", "Datos leídos del almacenamiento externo: $datosExternos")
+        } else {
+            //Para versiones anteriores de Android que requieren WRITE_EXTERNAL_STORAGE
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+                val filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).absolutePath + "/datos_externos.txt"
+                AlmacenamientoUtils.guardarEnAlmacenamientoExterno(filePath, "Algunos datos de prueba")
+
+                val datosExternos = AlmacenamientoUtils.leerDesdeAlmacenamientoExterno(filePath)
+                Log.d("Externo", "Datos leídos del almacenamiento externo: $datosExternos")
+            } else {
+                //Solicitar permisos para versiones anteriores si es necesario
+                requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), MY_PERMISSION_REQUEST_CODE)
+            }
+        }
+
+        //Almacenamiento interno
+        AlmacenamientoUtils.guardarEnAlmacenamientoInterno(this, "datos_internos.txt", "Almacenamiento interno")
+        val datosInternos = AlmacenamientoUtils.leerDesdeAlmacenamientoInterno(this, "datos_internos.txt")
+        Log.d("Interno", "Datos leídos del almacenamiento interno: $datosInternos")
+
         setContent {
             MainScreen { navigateToMainActivity() }
         }
+    }
+
+     fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions as Array<String>, grantResults)
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso concedido, continuar con el acceso al almacenamiento externo
+                val filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).absolutePath + "/datos_externos.txt"
+                AlmacenamientoUtils.guardarEnAlmacenamientoExterno(filePath, "Algunos datos de prueba")
+            }
+        }
+    }
+
+    companion object {
+        const val MY_PERMISSION_REQUEST_CODE = 1001
     }
 
     private fun navigateToMainActivity() {
